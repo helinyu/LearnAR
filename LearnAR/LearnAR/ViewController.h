f096d30e41b639c58ede87372d187931842f5179
felix <2319979647@qq.com>
2019-02-15 22:16:39 +0800

    fix

---
+++ LearnAR/LearnAR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnAR
+//
+//  Created by Aka on 2019/2/15.
+//  Copyright © 2019 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LearnAR
 this project code mainly left from learning AR 
+
+有关的内容，项目创建的过程等等，需要我们更加深入的去处理这个过程
+
